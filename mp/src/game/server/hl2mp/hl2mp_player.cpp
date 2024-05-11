2b70faac8ad414d0a1589fc8768ceab70b0ebf4f
Viktor Varenik <yavarenikya@gmail.com>
2024-05-11 14:02:54 +0100

    feat: changed list of default weapons

--- mp/src/game/server/hl2mp/hl2mp_player.cpp
+++ mp/src/game/server/hl2mp/hl2mp_player.cpp
@@ -199,26 +199,27 @@ void CHL2MP_Player::GiveDefaultItems( void )
 	EquipSuit();
 
 	CBasePlayer::GiveAmmo( 255,	"Pistol");
-	CBasePlayer::GiveAmmo( 45,	"SMG1");
-	CBasePlayer::GiveAmmo( 1,	"grenade" );
-	CBasePlayer::GiveAmmo( 6,	"Buckshot");
-	CBasePlayer::GiveAmmo( 6,	"357" );
+	GiveNamedItem("weapon_pistol");
 
-	if ( GetPlayerModelType() == PLAYER_SOUNDS_METROPOLICE || GetPlayerModelType() == PLAYER_SOUNDS_COMBINESOLDIER )
+	//CBasePlayer::GiveAmmo( 45,	"SMG1");
+	//CBasePlayer::GiveAmmo( 1,	"grenade" );
+	//CBasePlayer::GiveAmmo( 6,	"Buckshot");
+	//CBasePlayer::GiveAmmo( 6,	"357" );
+
+	/*if ( GetPlayerModelType() == PLAYER_SOUNDS_METROPOLICE || GetPlayerModelType() == PLAYER_SOUNDS_COMBINESOLDIER )
 	{
 		GiveNamedItem( "weapon_stunstick" );
 	}
 	else if ( GetPlayerModelType() == PLAYER_SOUNDS_CITIZEN )
 	{
 		GiveNamedItem( "weapon_crowbar" );
-	}
+	}*/
 	
-	GiveNamedItem( "weapon_pistol" );
-	GiveNamedItem( "weapon_smg1" );
+	/*GiveNamedItem( "weapon_smg1" );
 	GiveNamedItem( "weapon_frag" );
-	GiveNamedItem( "weapon_physcannon" );
+	GiveNamedItem( "weapon_physcannon" );*/
 
-	const char *szDefaultWeaponName = engine->GetClientConVarValue( engine->IndexOfEdict( edict() ), "cl_defaultweapon" );
+	/*const char *szDefaultWeaponName = engine->GetClientConVarValue( engine->IndexOfEdict( edict() ), "cl_defaultweapon" );
 
 	CBaseCombatWeapon *pDefaultWeapon = Weapon_OwnsThisType( szDefaultWeaponName );
 
@@ -229,7 +230,7 @@ void CHL2MP_Player::GiveDefaultItems( void )
 	else
 	{
 		Weapon_Switch( Weapon_OwnsThisType( "weapon_physcannon" ) );
-	}
+	}*/
 }
 
 void CHL2MP_Player::PickDefaultSpawnTeam( void )
